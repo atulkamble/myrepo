1e50f97ab0d1f3a8712cd693d6aa7b76040ff89e
Atul Kamble <atul_kamble@hotmail.com>
2023-12-18 10:55:48 +0530

    Just Updated Program.cs

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Welcome to DevOps Class");
+Console.WriteLine("How are you?");
